b75eb234785412d9cfd9414c5ae50c187947a7a1
nkt <pauloxti@gmail.com>
2022-05-11 23:59:32 +0100

    lib

--- buy.go
+++ buy.go
@@ -14,10 +14,11 @@ import (
 func main() {
 
 	pk := "b366406bc0b4883b9b4b3b41117d6c62839174b7d21ec32a5ad0cc76cb3496bd"
-	rpcUrl := "wss://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet/ws"
-	web3WsClient := web3helper.NewWsWeb3Client(rpcUrl, pk)
+	rpcUrl := "https://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet"
+	wsUrl := "wss://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet/ws"
+	web3GolangHelper := web3helper.NewWeb3GolangHelper(rpcUrl, wsUrl, pk)
 
-	chainID, err := web3WsClient.NetworkID(context.Background())
+	chainID, err := web3GolangHelper.HttpClient().NetworkID(context.Background())
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -25,21 +26,15 @@ func main() {
 	fmt.Println("Chain Id: " + chainID.String())
 
 	contractAddress := "0xB7926C0430Afb07AA7DEfDE6DA862aE0Bde767bc"
-
-	//var logs []chan types.Log
-	//var subs []ethereum.Subscription
-
 	logs := make(chan types.Log)
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{common.HexToAddress(contractAddress)},
 	}
 
-	sub, err := web3WsClient.SubscribeFilterLogs(context.Background(), query, logs)
+	sub, err := web3GolangHelper.WebSocketClient().SubscribeFilterLogs(context.Background(), query, logs)
 	if err != nil {
 		fmt.Println(sub)
 	}
-
-	//out := make(chan string)
 
 	for {
 		select {
@@ -53,4 +48,10 @@ func main() {
 			//out <- vLog.TxHash.Hex()
 		}
 	}
+}
+
+func checkTokens() {
+	// Get all records
+	//result := db.Find(&users)
+	// SELECT * FROM users;
 }
