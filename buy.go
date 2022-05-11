3c1834a4b50e7a78b56127d695f93a81fd4e0a18
nkt <pauloxti@gmail.com>
2022-05-11 23:14:29 +0100

    fix

--- buy.go
+++ buy.go
@@ -9,8 +9,9 @@ import (
 
 func main() {
 
-	rpcUrl := "https://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet"
-	web3HttpClient := web3helper.NewHttpWeb3Client(rpcUrl)
+	pk := "b366406bc0b4883b9b4b3b41117d6c62839174b7d21ec32a5ad0cc76cb3496bd"
+	rpcUrl := "wss://speedy-nodes-nyc.moralis.io/84a2745d907034e6d388f8d6/bsc/testnet/ws"
+	web3HttpClient := web3helper.NewWsWeb3Client(rpcUrl, pk)
 
 	chainID, err := web3HttpClient.NetworkID(context.Background())
 	if err != nil {
