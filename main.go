6af6f86528c28fb815c1cc79a5bb41fa68325ecb
drg809 <drg809@gmail.com>
2022-05-12 04:14:51 +0100

    merge

--- main.go
+++ main.go
@@ -59,19 +59,10 @@ func main() {
 
 	db := InitDatabase()
 	checkTokens(db)
-<<<<<<< HEAD
-	factoryAddress := "0xB7926C0430Afb07AA7DEfDE6DA862aE0Bde767bc" // pancakeFactory
-	logs := make(chan types.Log)
-	query := ethereum.FilterQuery{
-		Addresses: []common.Address{common.HexToAddress(factoryAddress)},
-	}
-=======
-	
+
 	contractAddress := "0x9ac64cc6e4415144c455bd8e4837fea55603e5c3"
 	logs := make(chan types.Log)
 	sub := BuildContractEventSubscription(web3GolangHelper, contractAddress, logs)
-
->>>>>>> 1e02325785ba734577be26862b4b6a82dd63cd48
 
 	contractAbi, _ := abi.JSON(strings.NewReader(string(pancakeFactory.PancakeABI)))
 
@@ -111,7 +102,7 @@ func main() {
 }
 
 func BuildContractEventSubscription(web3GolangHelper *web3helper.Web3GolangHelper, contractAddress string, logs chan types.Log) ethereum.Subscription {
-	
+
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{common.HexToAddress(contractAddress)},
 	}
